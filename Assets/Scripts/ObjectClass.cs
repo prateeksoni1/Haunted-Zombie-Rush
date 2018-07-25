6e7cb5f9b8a087648651167ac54eba044d42c95c
proforty7 <proforty7@gmail.com>
2018-07-25 15:35:18 +0530

    Platform position things

---
+++ Assets/Scripts/ObjectClass.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClass : MonoBehaviour {
+
+    [SerializeField] float objectSpeed = 1;
+    private float resetPosition = 42;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * objectSpeed * Time.deltaTime);
+
+        if (transform.localPosition.x <= resetPosition) {
+            Vector3 newPos = new Vector3(123, transform.position.y, transform.position.z);
+            transform.position = newPos;
+        }
+	}
+}
